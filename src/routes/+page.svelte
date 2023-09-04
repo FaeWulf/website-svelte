77b76363d07dfc3bb1cd7d79096820e8745a7517
Faewulf <ngolamaz3@gmail.com>
2023-09-04 21:48:53 +0700

    working on badges

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Badges from './badges.svelte';
+</script>
+
+<h1>👋 I'm Faewulf</h1>
+<Badges />
+
+<style>
+</style>
---
+++ src/routes/badges.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import badges from '$lib/bagde.json';
+	const skills = badges.skill;
+	skills.forEach((skill) => {
+		const url = new URL(`../lib/images/badges/${skill.name}.webp`, import.meta.url).href;
+		skill.url = url;
+	});
+</script>
+
+<div class="profile">
+	{#each skills as skill (skill.name)}
+		<img src={skill.url} alt={skill.name} />
+	{/each}
+</div>
+
+<style>
+	.profile {
+		display: flex;
+		padding: 5px;
+
+		width: 500px;
+		height: 300px;
+
+		flex-wrap: wrap;
+		overflow-y: auto;
+		overflow-x: hidden;
+
+		justify-content: flex-start;
+		justify-items: flex-start;
+		align-items: flex-start;
+		align-content: flex-start;
+
+		border-radius: 8px;
+
+		border: 1px rgba(var(--Text), 0.2) solid;
+		mask-image: linear-gradient(to bottom, black 70%, transparent 100%);
+		-webkit-mask-image: linear-gradient(to bottom, black 70%, transparent 100%);
+	}
+
+	img {
+		border-radius: 5px;
+		width: auto;
+		height: 28px;
+	}
+</style>
