c3c56c02b8beac56dc9fa3f644f50aa1f35d4fa6
Faewulf <ngolamaz3@gmail.com>
2023-10-18 14:55:06 +0700

    add some vars to handles boids setting

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -4,3 +4,15 @@ export const ufoBubble = writable("")
 
 export const apiURL = readable("https://faewulf.xyz")
 
+//3rd tab on window comp
+export const app3rd = writable("")
+export const S_boidsSettings = writable({
+    speed: 0.5,
+    visionRadius: 20,
+    visionAngle: 120,
+    localRadius: 80,
+    rivalRadius: 100
+})
+export const S_boidsCount = writable(0)
+export const S_boidChooseOption = writable("none")
+
