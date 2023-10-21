d3f0c38a354188a94cf6857ce280fc0140bc5549
Faewulf <ngolamaz3@gmail.com>
2023-10-21 14:10:06 +0700

    Fix the title

--- src/routes/projects/boids/+page.svelte
+++ src/routes/projects/boids/+page.svelte
@@ -2,6 +2,11 @@
 	import Canvas from '$lib/sveltes/page-comps/boids/canvas.svelte';
 </script>
 
+<svelte:head>
+	<title>Faewulf's Basement | Projects - Boids Simulator</title>
+	<meta name="description" content="About this website" />
+</svelte:head>
+
 <Canvas />
 
 <style lang="scss">
