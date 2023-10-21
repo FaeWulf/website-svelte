180c93cbfdc82ff3c397b4fa27c4887523dc2add
Faewulf <ngolamaz3@gmail.com>
2023-10-21 14:09:41 +0700

    fix some issue with shuffleFunctiion

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -105,10 +105,10 @@ function capitalFirstLeter(str: string) {
 
 
 //Fisher-Yates (Knuth) Shuffle Algorithm
-function shuffleArray(array: any[]) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
+function shuffleArray(array: unknown[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
         array[i] = array[j];
         array[j] = temp;
     }
