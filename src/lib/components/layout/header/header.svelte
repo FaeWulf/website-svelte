374a523e70c823af9c61369a57b98ea723ab50a3
FaeWulfs <ngolamaz3@gmail.com>
2024-12-04 12:12:14 +0700

    ~ Change: increase back-drop shadow.

--- src/lib/components/layout/header/header.svelte
+++ src/lib/components/layout/header/header.svelte
@@ -180,7 +180,7 @@
     //-webkit-backdrop-filter: blur(10px);
     border-radius: 50px;
     //background-color: rgba(var(--Overlay0), 0.6);
-    filter: drop-shadow(0px 0px 7px rgba(0, 0, 0, 0.2));
+    filter: drop-shadow(2px 2px 8px rgba(0, 0, 0, 0.8));
 
 
     .nav__toolbar {
