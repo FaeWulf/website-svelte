3156227733a683a4793cd04345441dbd616952e3
FaeWulfs <ngolamaz3@gmail.com>
2025-01-18 13:41:11 +0700

    # Fix: correct env for serverside.

--- src/routes/discord/+page.server.ts
+++ src/routes/discord/+page.server.ts
@@ -1,16 +1,16 @@
 import { redirect } from '@sveltejs/kit';
+import { PUBLIC_server } from '$env/static/public';
 
 export async function load() {
-	const fetchData = await fetch(process.env.PUBLIC_server + '/api/v1/discord/invite').then((res) => res.json());
+	const fetchData = await fetch(PUBLIC_server + '/api/v1/discord/invite').then((res) => res.json());
 
-	if (fetchData.data)
-		throw redirect(307, fetchData.data);
+	if (fetchData.data) throw redirect(307, fetchData.data);
 
 	return {
 		success: fetchData.data,
 		meta: {
 			title: 'Discord invite',
-			description: 'Invite link to faewulf\'s Discord',
+			description: "Invite link to faewulf's Discord",
 			keywords: ['discord invite', 'faewulf', 'discord']
 		}
 	};
